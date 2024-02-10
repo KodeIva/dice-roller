172cd0a6c88c63b943bbaf6896dd61d70f13d6c2
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-02-10 06:56:03 +0000

    Players divs added,dice images imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import dice1 from './images/dice1.png'
+import dice2 from './images/dice2.png'
+import dice3 from './images/dice3.png'
+import dice4 from './images/dice4.png'
+import dice5 from './images/dice5.png'
+import dice6 from './images/dice6.png'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 >Player 1 Wins!!! </h1>
+      <div>
+       <div>
+        <h2>Player 1</h2>
+         <img src={dice1} />
+       </div>
+       <div>
+        <h2>Player 2</h2>
+        <img src={dice2} />
+       </div>
+         <button>Roll Dices</button>
+      </div>
     </div>
   );
 }
